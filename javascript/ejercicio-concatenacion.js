38afca80caaa9e631383b7812ef926fa8da3e3ce
Reyna <al060872@uacam.mx>
2023-06-26 19:39:20 -0600

    Cree un documento JS para seguir practicando ejerciccios de concatenacion

---
+++ javascript/ejercicio-concatenacion.js
@@ -0,0 +1,9 @@
+// Al igual que podemos crear una cadena sobremúltiples líneas 
+// a partir de las cadenas literales, también podemos añadir variables 
+// a una cadena usando el operador "más igual" (+=)
+
+//Ejemplo
+// Cambia el código debajo de esta línea
+const someAdjective = "funny";
+let myStr = "Learning to code is ";
+myStr += someAdjective;
